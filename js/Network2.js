a13308ee44729bb01f973b9ddabff3af7df4be02
Sebastian Nyholm <sebn89@gmail.com>
2016-01-14 13:55:41 +0100

    removing c

--- js/Network2.js
+++ js/Network2.js
@@ -15,7 +15,6 @@ export default class Network {
       path: '/'
     })
     this.connectedPeers = {}
-    window.c = this.connectedPeers
     this.methods = {}
 
     this.peer.on('connection', this.initConnection)
